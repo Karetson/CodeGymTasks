11bdd2ee62769709db4ed8b4328a1338f721b60c
Karetson <robert.kareta@wp.pl>
2020-04-16 22:46:11 +0200

    level 3 done

--- 1.JavaSyntax/src/pl/codegym/task/task03/task0325/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0325/Solution.java
@@ -9,5 +9,11 @@ Oczekiwania finansowe
 public class Solution {
     public static void main(String[] args) throws Exception {
         //tutaj wpisz swój kod
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = reader.readLine();
+        int iN = Integer.parseInt(n);
+
+        System.out.println("Zarobię " + n + " zł na godzinę");
     }
 }
